beaa46e943433096ab07e96c2ca74d57e4a72889
esra2107 <150262332+esra2107@users.noreply.github.com>
2023-11-13 22:28:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+double faizoranı, anapara, gun;
+double faiz = 0;
+Console.Write("faizoranı giriniz");
+faizoranı= Convert.ToDouble(Console.ReadLine());
+Console.Write("anapara giriniz");
+anapara = Convert.ToDouble(Console.ReadLine());
+Console.Write("gun giriniz");
+gun = Convert.ToDouble(Console.ReadLine());
+faiz = (anapara * faizoranı * gun) / 360;
+Console.Write("faiz:" + faiz.ToString());
+Console.ReadKey();
