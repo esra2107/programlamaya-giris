2e671f20260b63d5f02ecc553a833a5744752bff
esra2107 <150262332+esra2107@users.noreply.github.com>
2023-11-14 23:19:59 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
-
-double faizoranı, anapara, gun;
-double faiz = 0;
-Console.Write("faizoranı giriniz");
-faizoranı= Convert.ToDouble(Console.ReadLine());
-Console.Write("anapara giriniz");
-anapara = Convert.ToDouble(Console.ReadLine());
-Console.Write("gun giriniz");
-gun = Convert.ToDouble(Console.ReadLine());
-faiz = (anapara * faizoranı * gun) / 360;
-Console.Write("faiz:" + faiz.ToString());
-Console.ReadKey();
+byte ay = Convert.ToByte(Console.ReadLine());
+switch (ay)
+{
+case 1:
+        Console.WriteLine("Ocak");
+        break;
+case 2:
+        Console.WriteLine("Şubat");
+        break;
+case 3:
+        Console.WriteLine("Mart");
+        break;
+ case 4:
+        Console.WriteLine("Nisan");
+        break;
+case 15:
+        Console.WriteLine("Mayıs");
+        break;
+case 6:
+        Console.WriteLine("Haziran");
+        break;
+case 7:
+        Console.WriteLine("Temmuz");
+        break;
+case 8:
+        Console.WriteLine("Ağustos");
+        break;
+case 9:
+        Console.WriteLine("Eylül");
+        break;
+case 10:
+        Console.WriteLine("Ekim");
+        break;
+case 11:
+        Console.WriteLine("Kasım");
+        break;
+case 12:
+        Console.WriteLine("Aralık");
+        break;
+default:
+        Console.WriteLine("1-12 arası sayı girmelisiniz!");
+        break;
+}
